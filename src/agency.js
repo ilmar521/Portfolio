0f5efb70b41066fd7f09427bce33ac7bdd81908c
ilmar521 <ilmar521@mail.ru>
2023-05-19 13:52:52 +0300

    commit

--- src/agency.js
+++ src/agency.js
@@ -24,3 +24,10 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var element = document.getElementById('loom-companion-mv3');
+    if (element) {
+      element.parentNode.removeChild(element);
+    }
+  });
